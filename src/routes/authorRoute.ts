c34e6a5eb7163062b66a8e76bb69722694659d19
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-09-19 21:27:58 +0200

    Added import for author controllers

--- src/routes/authorRoute.ts
+++ src/routes/authorRoute.ts
@@ -1,4 +1,5 @@
 import {Router, type Request, type Response} from "express";
+import {addAuthor, getAuthors, getAuthorById, updateAuthor, deleteAuthor} from "../controllers/authorController.js"
 
 const router = Router()
 
