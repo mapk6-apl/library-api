2b0cf7668d138d1dabcbc8084ab5c9567d6caea0
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-09-21 16:47:07 +0200

    Created a reusable validation middleware

--- src/routes/authorRoute.ts
+++ src/routes/authorRoute.ts
@@ -4,29 +4,28 @@ import {body, param, validationResult} from "express-validator"
 
 const router = Router()
 
-let authors: Array<{id?: number}> = [
-    {}
-]
-
 router.get('/', (req: Request, res: Response) => {
-    res.status(200).json(authors)
+    res.status(200).json(getAuthors())
 })
 
 router.get('/:id', [param('id').isInt().withMessage('Id must be an integer')], (req: Request, res: Response) => {
     const errors = validationResult(req)
 
-    console.log(errors, "errors from express validator middleware")
+    //console.log(errors, "errors from express validator middleware")
 
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
     }
 
     const {id} = req.params
-    const author = authors.find((author) => author.id === id)
+    const author = getAuthorById(Number(req.params.id)) //converts the string to a number
 
     if(!author){
         return res.status(404).send('Author not found')
     }
+
+    res.status(200).json(author)
+    return res.status(400).json({errors: errors.array()})
 })
 
 router.post('/', [
@@ -37,12 +36,10 @@ router.post('/', [
         return res.status(400).json({errors: errors.array()})
     }
 
-    console.log(req, 'request');
+    //console.log(req, 'request');
 
-    const [name, bookTitle] = req.body;
-    const newAuthor = {id: authors.length + 1, name, bookTitle};
-
-    authors.push(newAuthor)
+    const {name} = req.body;
+    const newAuthor = addAuthor(name);
 
     res.status(201).json(newAuthor);
 })
