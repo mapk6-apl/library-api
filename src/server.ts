51452cd1481775ef429d1f6637d6d051e926c5f7
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-09-22 16:16:09 +0200

    Mounted the error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, {type Express} from "express"
 import authorRoutes from './routes/authorRoute.js'
 import {loggerMiddleware} from "./middleware/logger.js" 
+import {errorHandler} from './middleware/errorHandlers.js'
+import {notFoundHandler} from './middleware/errorHandlers.js'
+
 
 const app: Express = express()
 const PORT = process.env.PORT || 3000 
@@ -10,6 +13,8 @@ app.use(express.json())
 
 app.use(loggerMiddleware)
 app.use('/authors', authorRoutes)
+app.use(notFoundHandler)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
