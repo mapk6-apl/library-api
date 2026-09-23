4aa154f2ea9dcb26009f7d10505dfc99a0adb8de
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-09-23 08:49:30 +0200

    Added GET, POST, PUT and DELETE routes

--- src/routes/bookRoute.ts
+++ src/routes/bookRoute.ts
@@ -1,5 +1,5 @@
 import {Router, type Request, type Response} from "express";
-import {addBook} from "../controllers/bookController.js"
+import {addBook, getBooks, getBookById, updateBook, deleteBook} from "../controllers/bookController.js"
 import {body, param, validationResult} from "express-validator"
 import {validate} from '../middleware/validate.js'
 
@@ -12,8 +12,31 @@ const bookRules = [
     body('year').isInt({min: 1, max: new Date().getFullYear()}).withMessage('year is invalid').toInt(),
 ]
 
+//add new book
 router.post('/', bookRules, validate, (req: Request, res: Response) => {
     const {title, authorId, year} = req.body
     res.status(201).json(addBook(title, authorId, year))
+})
+
+//get all books
+router.get('/', (req: Request, res: Response) => {
+    res.status(200).json(getBooks())
+})
+
+//get book by id
+router.get('/:id', idRule, validate, (req: Request, res: Response) => {
+    res.status(200).json(getBookById(Number(req.params.id)))
+})
+
+//update book details
+router.put('/:id', idRule, bookRules, validate, (req: Request, res: Response) => {
+    const {title, authorId, year} = req.body
+    res.status(200).json(updateBook(Number(req.params.id), title, authorId, year))
+})
+
+//delete books
+router.delete('/:id', idRule, validate, (req: Request, res: Response) => {
+    deleteBook(Number(req.params.id))
+    res.status(204).send()
 })
 
